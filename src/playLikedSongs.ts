6b1eca8376bddd514083e1be3c50a364cd92549d
Lucas Tenorio <tnrlucas@gmail.com>
2024-02-28 19:47:49 -0300

    Add shouldShuffleLikedSongs preference

--- src/playLikedSongs.ts
+++ src/playLikedSongs.ts
@@ -1,17 +1,20 @@
-import { showHUD } from "@raycast/api";
+import { getPreferenceValues, showHUD } from "@raycast/api";
 import { play } from "./api/play";
 import { setSpotifyClient } from "./helpers/withSpotifyClient";
 import { getMe } from "./api/getMe";
 import { shuffle } from "./api/shuffle";
 
 export default async function Command() {
+  const { shouldShuffleLikedSongs } = getPreferenceValues<{ shouldShuffleLikedSongs?: boolean }>();
+
   await setSpotifyClient();
 
   try {
     const meData = await getMe();
 
     console.log(meData?.id);
-    await shuffle(true);
+    if (shouldShuffleLikedSongs) await shuffle(true);
+
     play({ contextUri: `spotify:user:${meData?.id}:collection` });
 
     await showHUD("Playing Liked Songs");
